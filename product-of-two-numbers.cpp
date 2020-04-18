0a1134b71741d0a80216bddb02f3c2c065c68061
Rachi197 <63160859+Rachi197@users.noreply.github.com>
2020-04-18 05:31:33 +0530

    Add files via upload

---
+++ product-of-two-numbers.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float a,b,res;
+    cout<<"Enter 1st number\n";
+    cin>>a;
+    cout<<endl<<"Enter 2nd number\n";
+    cin>>b;
+    res=a*b;
+    cout<<endl<<"Product ="<<res;
+
+    return 0;
+}
